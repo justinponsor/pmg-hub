36cb5f914292e6c6b0a280bd988a29f28d077221
justinponsor <info@justinponsor.com>
2026-05-05 21:02:32 -0600

    Add files via upload

---
+++ api/axel-status.js
@@ -0,0 +1,10 @@
+// api/axel-status.js
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({
+    status: 'online',
+    replicate: !!process.env.REPLICATE_API_TOKEN,
+    storage: !!(process.env.SUPABASE_URL && process.env.SUPABASE_SERVICE_KEY),
+    version: '1.0.0',
+  });
+}
